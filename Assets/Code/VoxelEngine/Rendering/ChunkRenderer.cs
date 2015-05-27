0c304c2a10ef7c9b89afbf05224588c74ed56fca
Aler Denisov <aler.zampillo@gmail.com>
2015-05-27 06:51:03 +0300

    Lesson State: Cube is ready to fight!

--- Assets/Code/VoxelEngine/Rendering/ChunkRenderer.cs
+++ Assets/Code/VoxelEngine/Rendering/ChunkRenderer.cs
@@ -36,25 +36,69 @@ namespace Assets.Code.VoxelEngine.Rendering
             MeshFilter.mesh = GenerateMesh();
         }
 
+        private int[] GetQuadTriangles(ref int offset)
+        {
+            var quad = new[]
+            {
+                offset + 0, // first triangle
+                offset + 1,
+                offset + 2,
+                offset + 2, // seconds one
+                offset + 3,
+                offset + 0,
+            };
+
+            offset += 4;
+            return quad;
+        }
+
+        private Vector3[] GetQuadVertices()
+        {
+            return new[]
+            {
+                Vector3.left + Vector3.up + Vector3.back,
+                Vector3.right + Vector3.up + Vector3.back,
+                Vector3.right + Vector3.down + Vector3.back,
+                Vector3.left + Vector3.down + Vector3.back
+            };
+        }
+
         private Mesh GenerateMesh()
         {
             var mesh = new Mesh();
-            mesh.vertices = new[]
-            {
-                Vector3.left + Vector3.up,
-                Vector3.right + Vector3.up,
-                Vector3.right + Vector3.down,
-                Vector3.left + Vector3.down
-            };
+            var vertices = new List<Vector3>();
+            var triangles = new List<int>();
 
+            vertices.AddRange(RotateVertices(GetQuadVertices(), new Vector3(0, 0, 0)));
+            vertices.AddRange(RotateVertices(GetQuadVertices(), new Vector3(0, 90, 0)));
+            vertices.AddRange(RotateVertices(GetQuadVertices(), new Vector3(0, 180, 0)));
+            vertices.AddRange(RotateVertices(GetQuadVertices(), new Vector3(0, -90, 0)));
+            vertices.AddRange(RotateVertices(GetQuadVertices(), new Vector3(90, 0, 0)));
+            vertices.AddRange(RotateVertices(GetQuadVertices(), new Vector3(-90, 0, 0)));
 
-            mesh.triangles = new[]
-            {
-                0, 1, 2,
-                2, 3, 0
-            };
+            var offset = 0;
+            triangles.AddRange(GetQuadTriangles(ref offset));
+            triangles.AddRange(GetQuadTriangles(ref offset));
+            triangles.AddRange(GetQuadTriangles(ref offset));
+            triangles.AddRange(GetQuadTriangles(ref offset));
+            triangles.AddRange(GetQuadTriangles(ref offset));
+            triangles.AddRange(GetQuadTriangles(ref offset));
 
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            mesh.RecalculateNormals();
             return mesh;
+        }
+
+        private Vector3[] RotateVertices(Vector3[] vertices, Vector3 rotate)
+        {
+            var mtx = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(rotate), Vector3.one);
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                vertices[i] = mtx * vertices[i];
+            }
+
+            return vertices;
         }
     }
 }
